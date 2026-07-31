175429c07d91458be6c39a01751fed214ad83276
UsatovAI <xbajsjdjkaxbdj123456789@gmail.com>
2026-07-31 16:37:53 +0300

    Add Tools icon, rename Infra slug to infrastructure

--- components/ui/StackIcon.tsx
+++ components/ui/StackIcon.tsx
@@ -39,10 +39,19 @@ function InfraIcon() {
   );
 }
 
+function ToolsIcon() {
+  return (
+    <svg {...commonProps}>
+      <path d="M14.7 6.3a4 4 0 0 0-5.4 5.4L4 17l3 3 5.3-5.3a4 4 0 0 0 5.4-5.4l-2.2 2.2-2-2z" />
+    </svg>
+  );
+}
+
 const icons: Record<string, () => JSX.Element> = {
   backend: BackendIcon,
   "ml-python": MlPythonIcon,
-  infra: InfraIcon,
+  infrastructure: InfraIcon,
+  tools: ToolsIcon,
 };
 
 export function StackIcon({ slug }: { slug: string }) {
